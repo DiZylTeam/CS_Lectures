b6c897a9702ca0bd574fa71d7388d80ef7c9c790
Dmitry <9554300@gmail.com>
2023-05-29 18:17:14 +0300

    Add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username == "Mariya")
+{
+    Console.WriteLine("Wow!!! This is Mariya!!!");
+}
+else
+{
+    Console.Write ("Hello, ");
+    Console.WriteLine(username);
+}
